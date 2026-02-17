f000b32654aa65fe4b3382d3eb516e3339dfb1ff
bbm <brian.maranville@nist.gov>
2026-02-17 16:39:17 -0500

    adding test for element types

---
+++ test/test_element_types.py
@@ -0,0 +1,22 @@
+import periodictable
+from periodictable.core import Element, Isotope
+
+# Test symbol access
+c_element: Element = periodictable.C
+fe_element: Element = periodictable.Fe
+u_element: Element = periodictable.U
+
+# Test name access
+carbon: Element = periodictable.carbon
+iron: Element = periodictable.iron
+uranium: Element = periodictable.uranium
+
+# Test that we can access element properties
+mass: float = periodictable.C.mass
+symbol: str = periodictable.Fe.symbol
+number: int = periodictable.U.number
+
+# Test special cases
+deuterium: Element = periodictable.D
+tritium: Isotope = periodictable.T
+neutron: Element = periodictable.n
